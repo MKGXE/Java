09de21a8b2499144ca13398905aa625166e026d8
MKGXE <MKGXE>
2023-03-19 09:58:21 +0900

    2023.03.18 자바 1일

---
+++ Java01/src/operator/Oper2.java
@@ -0,0 +1,21 @@
+package operator;
+
+public class Oper2 {
+	public static void main(String[] args) {
+		int 국어 = 49;
+		int 영어 = 40;
+		int 수학 = 60;
+		
+		// 합계와 평균을 구하세요
+		
+		/*
+		 합계 : 000점
+		 평균 : 000점
+		 */
+		
+		int 합계 = 국어 + 영어 + 수학;
+		
+		System.out.println("합계 : "+(합계)+"점");
+		System.out.println("평균 : "+((double)합계/3)+"점");
+	}
+}
---
+++ Java01/src/variable/Type2.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class Type2 {
+	public static void main(String[] args) {
+		// 변수를 활용해서
+		// 이름, 출생년도, 키
+		
+		// 예시의 000을 변수로 활용
+		/*
+		 이름 : 000
+		 출생년도 : 0000년
+		 키 : 000.00cm
+		 */
+		
+		String name = "최민혁";
+		int year = 2007;
+		double height = 177.5;
+		
+		System.out.println("이름 : "+name);
+		System.out.println("출생년도 : "+year+"년");
+		System.out.println("키 : "+height+"cm");
+	}
+}
